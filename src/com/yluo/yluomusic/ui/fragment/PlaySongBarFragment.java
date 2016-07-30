b265f1d0d16f4e8dd50c06085e8d6600b224a2fd
yluo <yluotx@gmail.com>
2016-07-30 13:43:04 +0800

    把playSongbar抽取为一个fragment

---
+++ src/com/yluo/yluomusic/ui/fragment/PlaySongBarFragment.java
@@ -0,0 +1,31 @@
+package com.yluo.yluomusic.ui.fragment;
+
+import com.yluo.yluomusic.ui.fragment.base.BaseFragment;
+
+public class PlaySongBarFragment extends BaseFragment{
+
+	@Override
+	protected int getLayoutId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	protected void initUI() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initData() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void initEvent() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
